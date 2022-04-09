b35ade9bac7b271c6edac4f441503da4398d6a02
Batuhan Tomo <batuhan.tomo@gmail.com>
2022-04-10 00:37:05 +0300

    update

--- src/src/java/dao/KampanyaDAO.java
+++ src/src/java/dao/KampanyaDAO.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
 
-/**
- *
- * @author musabeytekin
- */
-public class KampanyaDAO {
-    
+import entity.Kampanya;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class KampanyaDAO extends DBConnection {
+
+    public void create(Kampanya k) {
+        try {
+            Statement st = this.connect().createStatement();
+            String query = "insert into kampanya (kampanya_id, indirim_yuzdesi) values('" + k.getKampanya_id() + "', '" + k.getIndirim_yuzdesi() + "') ";
+            st.executeUpdate(query);
+        } catch (Exception ex) {
+
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    public void update(Kampanya k) {
+        try {
+            Statement st = this.connect().createStatement();
+            String query = "update kampanya set kampanya_id = '" + k.getKampanya_id() + "', indirim_yuzdesi ='" + k.getIndirim_yuzdesi() + "' ";
+            st.executeUpdate(query);
+        } catch (Exception ex) {
+
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    public void delete(Kampanya k) {
+        try {
+            Statement st = this.connect().createStatement();
+            String query = "delete from kampanya where kampanya_id = '" + k.getKampanya_id() + "'";
+            st.executeUpdate(query);
+        } catch (Exception ex) {
+
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void getList() {
+        List<Kampanya> list = new ArrayList<>();
+        try {
+            Statement st = this.connect().createStatement();
+            String query = "select * from kampanya";
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                list.add(new Kampanya(rs.getInt("kampanya_id"), rs.getFloat("indirim_yuzdesi")));
+
+            }
+        } catch (Exception ex) {
+
+            System.out.println(ex.getMessage());
+        }
+    }
+
 }
