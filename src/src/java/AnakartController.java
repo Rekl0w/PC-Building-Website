228145516b6c9892f837d4190433c20422b87dfb
Zülfikar Babacan Elmalı <z.babacan.elmali@hotmail.com>
2022-04-10 01:37:11 +0300

    update

---
+++ src/src/java/AnakartController.java
@@ -0,0 +1,13 @@
+import jakarta.inject.Named;
+import jakarta.enterprise.context.Dependent;
+
+
+@Named(value = "anakartController")
+@Dependent
+public class AnakartController {
+
+    
+    public AnakartController() {
+    }
+    
+}
