42289e3aa1e7cc28027c580911dd2af232da0853
Batuhan Tomo <batuhan.tomo@gmail.com>
2022-04-10 00:06:14 +0300

    update

--- src/src/java/dao/KasaDAO.java
+++ src/src/java/dao/KasaDAO.java
@@ -1,13 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package dao;
 
-/**
- *
- * @author musabeytekin
- */
-public class KasaDAO {
-    
+import entity.Kasa;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class KasaDAO extends DBConnection {
+    public void create(Kasa k) {
+        try {
+            Statement st = this.connect().createStatement();
+            String query = "insert into kasa (kampanya_id, boyut, marka, fiyat, stok) values('" + k.getKampanya_id() + "', '" + k.getBoyut() + "', '" + k.getMarka() + "', '" + k.getFiyat() + "', '" + k.getStok() + "') ";
+            st.executeUpdate(query);
+        } catch (Exception ex) {
+
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    public void update(Kasa k) {
+        try {
+            Statement st = this.connect().createStatement();
+            String query = "update kasa set kampanya_id = '" + k.getKampanya_id() + "', boyut ='" + k.getBoyut() + "', marka = '" + k.getMarka() + "', fiyat = '" + k.getFiyat() + "', stok = '" + k.getStok() + "' ";
+            st.executeUpdate(query);
+        } catch (Exception ex) {
+
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    public void delete(Kasa k) {
+        try {
+            Statement st = this.connect().createStatement();
+            String query = "delete from kasa where urun_id = '" + k.getUrun_id() + "'";
+            st.executeUpdate(query);
+        } catch (Exception ex) {
+
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void getList() {
+        List<Kasa> list = new ArrayList<>();
+        try {
+            Statement st = this.connect().createStatement();
+            String query = "select * from kasa";
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                list.add(new Kasa(rs.getInt("kampanya_id"), rs.getString("boyut"), rs.getString("marka"), rs.getFloat("fiyat"), rs.getInt("stok")));
+
+            }
+        } catch (Exception ex) {
+
+            System.out.println(ex.getMessage());
+        }
+    }
+
 }
--- src/src/java/entity/Kasa.java
+++ src/src/java/entity/Kasa.java
@@ -4,18 +4,21 @@ package entity;
 
 public class Kasa extends BilgisayarBileseni{
     private String boyut;
+    private int kampanya_id;
 
     public Kasa() {
     }
 
-    public Kasa(String boyut, String marka, float fiyat, int stok) {
+    public Kasa(int kampanya_id, String boyut, String marka, float fiyat, int stok) {
         super(marka, fiyat, stok);
         this.boyut = boyut;
+        this.kampanya_id = kampanya_id;
     }
 
     public Kasa(String boyut, String marka, float fiyat) {
         super(marka, fiyat);
         this.boyut = boyut;
+        this.kampanya_id = kampanya_id;
     }
 
     public String getBoyut() {
@@ -24,6 +27,14 @@ public class Kasa extends BilgisayarBileseni{
 
     public void setBoyut(String boyut) {
         this.boyut = boyut;
+    }
+
+    public int getKampanya_id() {
+        return kampanya_id;
+    }
+
+    public void setKampanya_id(int kampanya_id) {
+        this.kampanya_id = kampanya_id;
     }
     
     
