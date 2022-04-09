24e24167e635e047c8d95e6b74d8e8b92e53a6c2
Zülfikar Babacan Elmalı <z.babacan.elmali@hotmail.com>
2022-04-10 01:38:34 +0300

    update

---
+++ src/src/java/controller/AnakartController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package controller;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.Dependent;
+
+/**
+ *
+ * @author zbaba
+ */
+@Named(value = "anakartController")
+@Dependent
+public class AnakartController {
+
+    /**
+     * Creates a new instance of AnakartController
+     */
+    public AnakartController() {
+    }
+    
+}
