c14214a05d13f5002455b6fd188f2a71efcfe447
Zülfikar Babacan Elmalı <z.babacan.elmali@hotmail.com>
2022-04-09 22:31:31 +0300

    update

--- src/src/java/dao/DBConnection.java
+++ src/src/java/dao/DBConnection.java
@@ -1,9 +1,25 @@
-
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-public class DBConnection {
-    
-    ///////
-    
-}
+public abstract class DBConnection {
+
+    public Connection connect() {
+        Connection c = null;
+
+        try {
+
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/pc_toplama", "postgres", "12345");
+           
+            
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+            }
+            return c;
+        }
+
+    }
+
+
