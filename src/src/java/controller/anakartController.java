e4d4f0ae3296404e1160a1e448a53fcbedebd4d7
musabeytekin <94857661+musabeytekin@users.noreply.github.com>
2022-04-10 01:32:59 +0300

    update

---
+++ src/src/java/controller/anakartController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.Dependent;
+
+public class anakartController {
+
+    /**
+     * Creates a new instance of anakartController
+     */
+    public anakartController() {
+    }
+
+}
