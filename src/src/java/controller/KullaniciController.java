6e8ba3f72653c99e4bc20458e7d1bb63ac0d30ed
Zülfikar Babacan Elmalı <z.babacan.elmali@hotmail.com>
2022-05-28 15:30:51 +0300

    update

--- src/src/java/controller/KullaniciController.java
+++ src/src/java/controller/KullaniciController.java
@@ -4,10 +4,14 @@ import dao.KullaniciDAO;
 import entity.Kullanici;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.validator.ValidatorException;
 import java.io.Serializable;
 import java.util.List;
 
-@Named(value ="kullaniciController")
+@Named(value = "kullaniciController")
 @SessionScoped
 public class KullaniciController implements Serializable {
 
@@ -15,9 +19,63 @@ public class KullaniciController implements Serializable {
     private KullaniciDAO dao;
     private List<Kullanici> list;
 
+    String buyukHarf = "(.*[A-Z].*)";
+    String kucukHarf = "(.*[a-z].*)";
+    String ozelKarakter = "(.*[@,#,$,%,_,-,.,,,+].*$)";
+
+    public boolean validatePassword(FacesContext context, UIComponent cmp, Object value) throws ValidatorException {
+        String v = (String) value;
+
+        if (v.isEmpty()) {
+            throw new ValidatorException(new FacesMessage("Şifre Alanı Boş Olamaz."));
+
+        } else if (v.length() < 8) {
+
+            throw new ValidatorException(new FacesMessage("En az 8 karakter girmeliniz."));
+
+        } else if (v.length() > 15) {
+
+            throw new ValidatorException(new FacesMessage("En fazla 15 karakter girebilirsiniz."));
+
+        } else if (!v.matches(buyukHarf)) {
+            throw new ValidatorException(new FacesMessage("Sifrenizde en az 1 tane büyük harf olmalıdır."));
+
+        } else if (!v.matches(kucukHarf)) {
+
+            throw new ValidatorException(new FacesMessage("Sifrenizde en az 1 tane küçük harf olmalıdır."));
+
+        } else if (!v.matches(ozelKarakter)) {
+
+            throw new ValidatorException(new FacesMessage("Sifrenizde en az 1 tane özel karakter olmalıdır."));
+
+        }
+
+        return true;
+    }
+
+    public boolean validateUser(FacesContext context, UIComponent cmp, Object value) throws ValidatorException {
+        String v = (String) value;
+
+        if (v.isEmpty()) {
+            throw new ValidatorException(new FacesMessage("İsim Alanı Boş Olamaz."));
+
+        } else if (v.length() < 5) {
+
+            throw new ValidatorException(new FacesMessage("En az 5 karakter girmeliniz."));
+
+        } else if (v.matches(ozelKarakter)) {
+
+            throw new ValidatorException(new FacesMessage("İsiminizde özel karakter olamaz."));
+
+        }
+        return true;
+    }
+
     public void create() {
         this.getDao().create(entity);
         entity = new Kullanici();
+
+        System.out.println("--------------REGİSTERED-----------");
 
     }
 
